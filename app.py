dab7ca7056a86009f25a846200320b59704f6852
sir-meee <sammymbevi@gmail.com>
2018-11-25 08:29:08 +0300

    add blog table

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:////home/sammy/Documents/Python/F
 
 db = SQLAlchemy(app)
 
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
 @app.route('/')
 def index():
     return render_template('index.html')
