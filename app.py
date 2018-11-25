fcec0b8c7fee5430123ca98ffbbb73e055c557af
sir-meee <sammymbevi@gmail.com>
2018-11-25 15:07:25 +0300

    include add post app route

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def about():
 def post():
     return render_template('post.html')
 
-@app.route('/contact') 
-def contact():
-    return render_template('contact.html')
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
