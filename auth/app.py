ecdf9dff0a2e6e56831867f6a67a8c4486ea8180
sir-meee <sammymbevi@gmail.com>
2018-11-27 11:11:14 +0300

    create register class and pass it to the render template

--- auth/app.py
+++ auth/app.py
@@ -1,10 +1,11 @@
-rom flask import Flask, render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
 Bootstrap(app)
 
 class LoginForm(FlaskForm):
@@ -13,10 +14,15 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 
 @app.route('/')
-def index():
+def index():/home/sammy/Documents/Python/Flask
     return render_template('index.html')
 
 @app.route('/login')
@@ -27,7 +33,8 @@ def login():
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    form = RegisterForm()
+    return render_template('signup.html', form=form)
 
 @app.route('/dashboard')
 def dashboard():
