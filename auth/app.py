0bbf785dc330ab01f05293a6e944170bbf921009
sir-meee <sammymbevi@gmail.com>
2018-11-27 10:13:34 +0300

    add routes to login pages

--- auth/app.py
+++ auth/app.py
@@ -4,3 +4,19 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
